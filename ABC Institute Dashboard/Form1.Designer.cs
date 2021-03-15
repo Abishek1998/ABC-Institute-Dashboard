99bd59beaeb871cf2508a2a18b0d5755ecf60a16
Abishek1998 <abishaj1998@gmail.com>
2021-03-15 22:48:19 +0530

    Left and Right Panel

--- ABC Institute Dashboard/Form1.Designer.cs
+++ ABC Institute Dashboard/Form1.Designer.cs
@@ -30,9 +30,10 @@ namespace ABC_Institute_Dashboard
         private void InitializeComponent()
         {
             this.gunaPanel1 = new Guna.UI.WinForms.GunaPanel();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.gunaPanel2 = new Guna.UI.WinForms.GunaPanel();
             this.gunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
-            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.gunaPanel3 = new Guna.UI.WinForms.GunaPanel();
             this.gunaPanel1.SuspendLayout();
             this.gunaPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).BeginInit();
@@ -48,6 +49,15 @@ namespace ABC_Institute_Dashboard
             this.gunaPanel1.Name = "gunaPanel1";
             this.gunaPanel1.Size = new System.Drawing.Size(250, 729);
             this.gunaPanel1.TabIndex = 0;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 100);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
+            this.guna2Panel1.Size = new System.Drawing.Size(250, 180);
+            this.guna2Panel1.TabIndex = 1;
             // 
             // gunaPanel2
             // 
@@ -70,13 +80,14 @@ namespace ABC_Institute_Dashboard
             this.gunaPictureBox1.TabIndex = 0;
             this.gunaPictureBox1.TabStop = false;
             // 
-            // guna2Panel1
+            // gunaPanel3
             // 
-            this.guna2Panel1.Location = new System.Drawing.Point(24, 142);
-            this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
-            this.guna2Panel1.Size = new System.Drawing.Size(200, 100);
-            this.guna2Panel1.TabIndex = 1;
+            this.gunaPanel3.BackColor = System.Drawing.Color.White;
+            this.gunaPanel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.gunaPanel3.Location = new System.Drawing.Point(256, 0);
+            this.gunaPanel3.Name = "gunaPanel3";
+            this.gunaPanel3.Size = new System.Drawing.Size(1094, 729);
+            this.gunaPanel3.TabIndex = 1;
             // 
             // MainDashboard
             // 
@@ -84,6 +95,7 @@ namespace ABC_Institute_Dashboard
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
             this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.gunaPanel3);
             this.Controls.Add(this.gunaPanel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -102,6 +114,7 @@ namespace ABC_Institute_Dashboard
         private Guna.UI.WinForms.GunaPanel gunaPanel2;
         private Guna.UI.WinForms.GunaPictureBox gunaPictureBox1;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI.WinForms.GunaPanel gunaPanel3;
     }
 }
 
